0bc7138df0e3bd538fe1b2c8bcc2dd229c7cc4b9
Krusovice <jmk1589@gmail.com>
2025-05-02 15:05:42 +0200

    equalizing scales in plots and added tests

--- src/output/plots.rs
+++ src/output/plots.rs
@@ -80,11 +80,39 @@ fn plot_canvas<'a>(kp_list:&[Keypoint],
                    output_path:&'a str,
                    dimension: (u32, u32),
                    chart_title:&str) -> ChartContext<'a, BitMapBackend<'a>, Cartesian2d<RangedCoordf32, RangedCoordf32>> {
-                   
+    
+    // Setting an equal x and y scale.      
     let min_x:f32 = kp_list.iter().map(|kp| kp.x as f32).fold(f32::INFINITY, |acc, x| f32::min(acc,x));
     let max_x:f32 = kp_list.iter().map(|kp| kp.x as f32).fold(f32::NEG_INFINITY, |acc, x| f32::max(acc,x));
     let min_y:f32 = kp_list.iter().map(|kp| kp.y as f32).fold(f32::INFINITY, |acc, y| f32::min(acc,y));
     let max_y:f32 = kp_list.iter().map(|kp| kp.y as f32).fold(f32::NEG_INFINITY, |acc, y| f32::max(acc,y));
+
+    // Defining pixels for calculating scale
+    let (width_px, height_px) = dimension;
+    
+    // Finding the ranges
+    let x_range = max_x - min_x;
+    let y_range = max_y - min_y;
+
+    // Finding the scale for x and y
+    let x_scale = x_range / width_px as f32;
+    let y_scale = y_range / height_px as f32;
+
+    // Use the larger scale to make both axes "equal"
+    let uniform_scale = x_scale.max(y_scale);
+
+    // Finding the chart center
+    let x_center = (min_x + max_x) / 2.0;
+    let y_center = (min_y + max_y) / 2.0;
+
+
+    let half_width = (uniform_scale * width_px as f32) / 2.0;
+    let half_height = (uniform_scale * height_px as f32) / 2.0;
+
+    let min_x = x_center - half_width;
+    let max_x = x_center + half_width;
+    let min_y = y_center - half_height;
+    let max_y = y_center + half_height;
 
     let backend = BitMapBackend::new(output_path, dimension);
     let drawing_area = backend.into_drawing_area();
